266b25a2375b3663bfd57be799b70b254f1d99bd
Mohammad Ibrahim <112249309+awesomecoderdev@users.noreply.github.com>
2023-09-07 18:39:12 +0600

    Updated UI

--- template/section/category/header.php
+++ template/section/category/header.php
@@ -1,11 +1,10 @@
 <section id="categories" class="relative border-y border-primary-50/50 dark:border-slate-50/25 text-slate-500 dark:text-white">
-    <div class="relative container text-sm font-normal py-2 flex justify-evenly items-center">
-
+    <div class="relative container text-sm font-normal py-2 flex flex-wrap justify-evenly items-center">
         <?php foreach (get_lumi_categories([
             "number" => 12,
             'parent'        => 0,
         ]) as $key => $category) : ?>
-            <a href="<?php echo get_term_link($category); ?>"><?php echo $category->name; ?></a>
+            <a class="px-2" href="<?php echo get_term_link($category); ?>"><?php echo $category->name; ?></a>
         <?php endforeach; ?>
     </div>
 </section>
