dec6f4c03ad7f00cec4649e4109a04ca7d7582fb
Mohammad Ibrahim <112249309+awesomecoderdev@users.noreply.github.com>
2023-09-08 04:27:23 +0600

    Updated Wishlist

--- page-wishlist.php
+++ page-wishlist.php
@@ -107,7 +107,7 @@ $products = lumi_get_products([
                 <!-- start for large device -->
                 <div class="relative flex justify-between items-end rounded-lg border p-4 <?php echo wp_is_mobile() ? "hidden" : "" ?>">
                     <div class="relative h-full flex items-center gap-4">
-                        <div class="relative glass rounded-full flex justify-center items-center p-1 text-white">
+                        <div class="relative glass rounded-full flex justify-center items-center p-1 text-primary-500 cursor-pointer" id="add-to-wishlist" data-product="<?php the_ID() ?>">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 product-wishlist-item product-wishlist-<?php the_ID() ?> text-red-500">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
                             </svg>
