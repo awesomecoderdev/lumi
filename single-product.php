cfb77d8ef03e451993a5770bc8a40de8fa29c564
Mohammad Ibrahim <awesomecoder.dev@gmail.com>
2023-10-02 13:24:33 +0600

    Updated UI

--- single-product.php
+++ single-product.php
@@ -201,10 +201,10 @@ if (!defined('ABSPATH')) {
 
 					<?php if ($product?->is_in_stock()) : ?>
 						<p class="text-zinc-800 font-semibold"><?php _e("Available in stock.", "lumi") ?></p>
-						<?php //do_action("woocommerce_single_product_cart")
-						?>
+						<?php do_action("woocommerce_single_product_cart") ?>
 					<?php else : ?>
 						<p class="text-zinc-800 font-semibold"><?php _e("Out of stock.", "lumi") ?></p>
+						<?php do_action("woocommerce_single_product_cart") ?>
 					<?php endif; ?>
 				</div>
 			</div>
@@ -213,9 +213,9 @@ if (!defined('ABSPATH')) {
 
 	<div class="relative py-4">
 		<div class="relative px-4 py-2 border border-primary-500 flex items-center justify-between rounded-2xl">
-			<a class="block bg-primary-500 text-white py-2 px-5 min-w-[10rem] text-center rounded-full single-product-section-link" href="#general"><?php _e("General Information", "lumi"); ?></a>
-			<a class="block bg-primary-500 text-white py-2 px-5 min-w-[10rem] text-center rounded-full single-product-section-link" href="#reviews"><?php _e("Additional Information", "lumi"); ?></a>
-			<a class="block bg-primary-500 text-white py-2 px-5 min-w-[10rem] text-center rounded-full single-product-section-link" href="#reviews"><?php _e("Reviews", "lumi"); ?></a>
+			<a class="block py-2 px-5 min-w-[10rem] text-center rounded-full single-product-section-link active" href="#general"><?php _e("General Information", "lumi"); ?></a>
+			<a class="block py-2 px-5 min-w-[10rem] text-center rounded-full single-product-section-link" href="#info"><?php _e("Additional Information", "lumi"); ?></a>
+			<a class="block py-2 px-5 min-w-[10rem] text-center rounded-full single-product-section-link" href="#reviews"><?php _e("Reviews", "lumi"); ?></a>
 		</div>
 	</div>
 
@@ -226,13 +226,38 @@ if (!defined('ABSPATH')) {
 			</div>
 
 			<div id="reviews" class="single-product-section" style="display: none;">
+				<div class="relative space-y-4">
+					<span class="text-2xl font-semibold"><?php _e("Customer Reviews", "lumi"); ?></span>
+					<div class="relative flex items-center">
+						<div class="relative flex text-yellow-100 w-fit overflow-hidden">
+							<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+								<path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+							</svg>
+							<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+								<path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+							</svg>
+							<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+								<path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+							</svg>
+							<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+								<path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+							</svg>
+							<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+								<path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+							</svg>
+							<div class="absolute bg-yellow-900 top-0 left-0 bottom-0 right-0 mix-blend-color-burn" style="width:<?php echo ($product->get_average_rating() / 5 * 100); ?>% ;"></div>
+						</div>
+						<span class="text-sm font-semibold m-0 px-2"><span class="font-extrabold mx-1"><?php echo $product->get_average_rating(); ?></span> <?php echo $product->get_rating_count() ? "(Total {$product->get_rating_count()} reviews)" : ""; ?></span>
+					</div>
+				</div>
+
 				<?php if ($product->get_rating_count() > 0) : ?>
 					<div class="woocommerce-product-reviews">
 						<div class="woocommerce-product-rating">
-							<?php echo wc_get_rating_html($product->get_average_rating()); ?>
+							<!-- <?php echo wc_get_rating_html($product->get_average_rating()); ?>
 							<span class="review-count">
 								(<?php echo $product->get_rating_count(); ?> <?php _e('reviews', 'woocommerce'); ?>)
-							</span>
+							</span> -->
 						</div>
 						<?php
 						$args = array(
@@ -246,11 +271,15 @@ if (!defined('ABSPATH')) {
 
 						if ($reviews) :
 							foreach ($reviews as $review) :
-								echo '<div class="review">';
-								echo '<p class="review-author">' . esc_html($review->comment_author) . '</p>';
-								echo '<div class="review-rating">' . wc_get_rating_html(intval(get_comment_meta($review->comment_ID, 'rating', true))) . '</div>';
-								echo '<p class="review-text">' . esc_html($review->comment_content) . '</p>';
-								echo '</div>';
+							// echo "<pre>";
+							// print_r($review);
+							// echo "</pre>";
+
+							// echo '<div class="review">';
+							// echo '<p class="review-author">' . esc_html($review->comment_author) . '</p>';
+							// echo '<div class="review-rating">' . wc_get_rating_html(intval(get_comment_meta($review->comment_ID, 'rating', true))) . '</div>';
+							// echo '<p class="review-text">' . esc_html($review->comment_content) . '</p>';
+							// echo '</div>';
 							endforeach;
 						else :
 							echo '<p>' . __('There are no reviews yet.', 'woocommerce') . '</p>';
@@ -270,6 +299,9 @@ if (!defined('ABSPATH')) {
 				try {
 					$(".single-product-section").hide();
 					$(`${window.location.hash}`).show();
+
+					$(".single-product-section-link").removeClass("active");
+					$(`a[href='${window.location.hash}'].single-product-section-link`).addClass("active");
 				} catch (error) {
 					// skip
 				}
@@ -281,6 +313,9 @@ if (!defined('ABSPATH')) {
 					if (target && target.startsWith("#")) {
 						$(".single-product-section").hide();
 						$(`${target}`).show();
+
+						$(".single-product-section-link").removeClass("active");
+						$(`a[href='${target}'].single-product-section-link`).addClass("active");
 					}
 				} catch (error) {
 					// skip
